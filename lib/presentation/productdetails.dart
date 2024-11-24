d2c73a26844dda5beaa5ddd227f57458fc5a93ed
Matt <gwadabethwel@gmail.com>
2024-11-25 00:49:32 +0300

    added details to product

--- lib/presentation/productdetails.dart
+++ lib/presentation/productdetails.dart
@@ -9,6 +9,7 @@ class Productdetails extends StatefulWidget {
 }
 
 class _ProductdetailsState extends State<Productdetails> {
+  int x = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +28,7 @@ class _ProductdetailsState extends State<Productdetails> {
               ),
             ),
             Image.asset(
-              "Images/Jina_Trainer.png",
+              "Images/steak_potato.png",
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height / 2.5,
               fit: BoxFit.fill,
@@ -35,20 +36,143 @@ class _ProductdetailsState extends State<Productdetails> {
             SizedBox(
               height: 10.0,
             ),
-            Text(
-              "Jina - Personal Trainer",
-              style: AppWidget.boldTextFieledStyle(),
-            ),
             Row(
               children: [
-                Container(
-                  decoration: BoxDecoration(),
-                  child: Icon(
-                    Icons.remove,
-                    color: Colors.white,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Grilled Steak and\n Fried Potatoes",
+                      style: AppWidget.boldTextFieledStyle(),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    if (x > 1) {
+                      --x;
+                    }
+
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text(
+                  x.toString(),
+                  style: AppWidget.smallBoldTextFieledStyle(),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    ++x;
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "This dish comprises of fried beef escorted with fried potato chops. Some of its constituent nutrients include: protein, fats, omega 3 and 6 fatty acids, Vitamins B12, B6, B3, B2, B1 and minerals such as zinc, selenium, phosphorous, L-arginine, potassium and magnesium, all within the beef. As for the fried potatoes, they contain: Complex carbohydrates and simple sugars, fibres, fats, vitamins B6, C, B3, B2, B1 and minerals such as potassium, magnesium, phosphrous, iron, zinc, calcium and selenium.",
+              style: AppWidget.smallBoldTextFieledStyle(),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Delivery Time:",
+                  style: AppWidget.smallBoldTextFieledStyle(),
+                ),
+                Icon(
+                  Icons.alarm,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "15 minutes ",
+                  style: AppWidget.smallBoldTextFieledStyle(),
+                ),
+              ],
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Price:",
+                        style: AppWidget.smallBoldTextFieledStyle(),
+                      ),
+                      Text(
+                        "\Ksh750",
+                        style: AppWidget.boldTextFieledStyle(),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Add to cart",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                          child: Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
